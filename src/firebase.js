c7d382f4568d0c85f563b0dda3737f610dda998f
avarnie <84094753+avarnie@users.noreply.github.com>
2022-12-07 10:38:04 +0000

    I added firebase to our project, to fitch data from the database

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDWnJa1qr2HnhGJ_tDVm2zTsOsSAhHt0l0",
-    authDomain: "lssei-710b1.firebaseapp.com",
-    databaseURL: "https://lssei-710b1-default-rtdb.firebaseio.com",
-    projectId: "lssei-710b1",
-    storageBucket: "lssei-710b1.appspot.com",
-    messagingSenderId: "424843453707",
-    appId: "1:424843453707:web:a9bce0664fee81d51eee0e"
-  };
+  apiKey: "AIzaSyDWnJa1qr2HnhGJ_tDVm2zTsOsSAhHt0l0",
+  authDomain: "lssei-710b1.firebaseapp.com",
+  databaseURL: "https://lssei-710b1-default-rtdb.firebaseio.com",
+  projectId: "lssei-710b1",
+  storageBucket: "lssei-710b1.appspot.com",
+  messagingSenderId: "424843453707",
+  appId: "1:424843453707:web:a9bce0664fee81d51eee0e"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default firebase;
+export const firestoreDB = getFirestore(app);
+const analytics = getAnalytics(app);
