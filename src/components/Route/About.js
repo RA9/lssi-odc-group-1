ea4e2dcc450e377875093eb0567cae6001630ca0
Philip Marcus Nathan Jr <115917564+nathan310@users.noreply.github.com>
2022-12-07 12:37:17 +0000

    about page

---
+++ src/components/Route/About.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+export default function About(){
+    return (
+        <>
+        <div className="e-card">
+    <div className="e-card-image">
+      <div className="e-card-title">JavaScript </div>
+    </div>
+    <div className="e-card-content">
+      JavaScript Succinctly was written to give readers an accurate, concise examination of JavaScript objects and their supporting nuances, such as complex values, primitive values, scope, inheritance, the head object, and more. </div>
+  </div>
+
+  <div className="e-card">
+    <div className="e-card-image">
+      <div className="e-card-title">JavaScript </div>
+    </div>
+    <div className="e-card-content">
+      JavaScript Succinctly was written to give readers an accurate, concise examination of JavaScript objects and their supporting nuances, such as complex values, primitive values, scope, inheritance, the head object, and more. </div>
+  </div>
+
+  <div className="e-card">
+    <div className="e-card-image">
+      <div className="e-card-title">JavaScript </div>
+    </div>
+    <div className="e-card-content">
+      JavaScript Succinctly was written to give readers an accurate, concise examination of JavaScript objects and their supporting nuances, such as complex values, primitive values, scope, inheritance, the head object, and more. </div>
+  </div>
+
+  <div className="e-card">
+    <div className="e-card-image">
+      <div className="e-card-title">JavaScript </div>
+    </div>
+    <div className="e-card-content">
+      JavaScript Succinctly was written to give readers an accurate, concise examination of JavaScript objects and their supporting nuances, such as complex values, primitive values, scope, inheritance, the head object, and more. </div>
+  </div>
+
+  <div className="e-card">
+    <div className="e-card-image">
+      <div className="e-card-title">JavaScript </div>
+    </div>
+    <div className="e-card-content">
+      JavaScript Succinctly was written to give readers an accurate, concise examination of JavaScript objects and their supporting nuances, such as complex values, primitive values, scope, inheritance, the head object, and more. </div>
+  </div>
+  </>
+  );
+}
